5f441a2201bfbf6a91b641095922f9acc8dfdeb4
Purrukatte <ozcanozgur@live.com>
2025-07-12 00:25:48 +0300

    index.html ve vite.config.js dosyaları düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({
     // JSX transformasyonunu React ile açıkça yapılandıralım
     jsxRuntime: 'automatic',
-    // JSX importlarını otomatik ekletelim
-    include: '**/*.js',
+    // JSX dosyalarını işleyelim
+    include: '**/*.jsx',
   })],
   base: '/campaign-analyzer/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 })
