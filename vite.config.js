a024aea60d26c1709fcee314414a7c3b96e419b3
Purrukatte <ozcanozgur@live.com>
2025-07-12 00:22:44 +0300

    JSX dosya uzantilari düzeltildi, babel yapilandirmasi eklendi

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // JSX transformasyonunu React ile açıkça yapılandıralım
+    jsxRuntime: 'automatic',
+    // JSX importlarını otomatik ekletelim
+    include: '**/*.js',
+  })],
   base: '/campaign-analyzer/',
   build: {
     outDir: 'dist'
-  },
-  // TypeScript'i atlayarak direkt JSX dosyalarıyla çalışalım
-  esbuild: {
-    jsx: 'automatic',
-    jsxInject: `import React from 'react'`
   }
 })
