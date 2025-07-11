fec5f21d79fff963699b6c17e06d32b45a69c71e
Purrukatte <ozcanozgur@live.com>
2025-07-12 00:06:16 +0300

    TypeScript'ten JavaScript'e geçiş yapıldı ve BOM sorunu için build süreci düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/campaign-analyzer/',
   build: {
     outDir: 'dist'
+  },
+  // TypeScript'i atlayarak direkt JSX dosyalarıyla çalışalım
+  esbuild: {
+    jsx: 'automatic',
+    jsxInject: `import React from 'react'`
   }
 })
